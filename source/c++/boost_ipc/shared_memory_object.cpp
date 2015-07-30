060a5d3ffc03bee2600f871a68ab11015a3a672b
Edison Gustavo Muenz <muenz@esss.com.br>
2015-07-30 15:16:52 -0300

    added bindings to shared_memory_object

---
+++ source/c++/boost_ipc/shared_memory_object.cpp
@@ -0,0 +1,30 @@
+#include <boost/python.hpp>
+#include <boost/interprocess/ipc/message_queue.hpp>
+#include <boost/shared_ptr.hpp>
+
+using namespace boost::interprocess;
+using namespace boost::python;
+
+void register_shared_memory_object() {
+    typedef shared_memory_object T;
+    class_<T, boost::shared_ptr<T>, boost::noncopyable> c("shared_memory_object", init<>());
+    c.def(
+        init<create_only_t, const char *, mode_t, optional<const permissions&>>(
+            args("tag", "name", "mode", "permissions")
+        )
+    );
+    c.def(
+        init<open_or_create_t, const char *, mode_t, optional<const permissions&>>(
+            args("tag", "name", "mode", "permissions")
+        )
+    );
+    c.def(
+        init<open_only_t, const char *, mode_t>(
+            args("tag", "name", "mode")
+        )
+    );
+    c.def("remove", &T::remove);
+    c.staticmethod("remove");
+
+    //TODO: finish "shared_memory_object"
+}
